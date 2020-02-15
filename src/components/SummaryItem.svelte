d0dd343e50c7d2e5e11aeb8156e93935a5eba195
koneko096 <laser.survivor@gmail.com>
2020-02-15 16:52:17 +0700

    format number properly

--- src/components/SummaryItem.svelte
+++ src/components/SummaryItem.svelte
@@ -4,7 +4,38 @@
   export let created_at;
   export let preview_image;
   export let view_count = 0;
-  const subtitleText = `${owner.name} - ${view_count} views - ${created_at} seconds ago`;
+
+  function generateDateDifferences(creationTimestamp) {
+    let diff = Math.floor((Date.now() - creationTimestamp) / 1000);
+    if (diff < 60) {
+      return `${diff} seconds`;
+    }
+    diff = Math.floor(diff / 60);
+    if (diff < 60) {
+      return `${diff} minutes`;
+    }
+    diff = Math.floor(diff / 60);
+    if (diff < 24) {
+      return `${diff} hours`;
+    }
+    diff = Math.floor(diff / 24);
+    if (diff < 30) {
+      return `${diff} days`;
+    }
+    if (diff < 365) {
+      return `${Math.floor(diff / 30)} months`;
+    }
+    diff = Math.floor(diff / 365);
+    return `${diff} years`;
+  }
+
+  function shortenNumber(num) {
+    return Math.abs(num) > 999 ? 
+      Math.sign(num) * ((Math.abs(num) / 1000).toFixed(1)) + 'k' : 
+      num;
+  }
+
+  const subtitleText = `${owner.name} - ${shortenNumber(view_count)} views - ${generateDateDifferences(created_at)} ago`;
 </script>
 
 <style>
